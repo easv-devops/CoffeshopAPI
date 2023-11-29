e86a0692ca65fdb1b45987da68415f4f2b91c1df
chrsut01 <chrissu360@gmail.com>
2023-11-29 12:29:32 +0100

    uh

---
+++ Models/Entities/DTOs/CreateCoffeeBeanDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Models.Entities;
+
+public partial class CreateCoffeeBeanDTO
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string Description { get; set; } = null!;
+}
